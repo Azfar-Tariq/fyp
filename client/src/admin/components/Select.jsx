14b7f7cfb4801a4054801c3f8905543872b668f4
Azfar-Tariq <azfaro2031@gmail.com>
2024-02-21 00:18:21 +0500

    More changes

--- client/src/admin/components/Select.jsx
+++ client/src/admin/components/Select.jsx
@@ -1,43 +1,48 @@
 import { useState } from "react";
 import { nodes } from "../data";
+import { IcOutlineKeyboardArrowDown } from "../assets/icons/down";
+import { MaterialSymbolsArrowForwardIosRounded } from "../assets/icons/foward";
 
 export default function Select() {
   const [selectedArea, setSelectedArea] = useState(null);
 
-  const handleAreaChange = (event) => {
-    setSelectedArea(event.target.value);
+  const handleAreaClick = (areaName) => {
+    setSelectedArea(areaName === selectedArea ? null : areaName);
   };
 
   return (
     <div className="flex flex-col">
-      <label htmlFor="area">Select Area</label>
-      <select
-        id="area"
-        name="area"
-        className="border border-gray-300 rounded-md"
-        onChange={handleAreaChange}
-        value={selectedArea}
-      >
-        <option value="">All Areas</option>
+      <h2 className="text-lg font-semibold">Areas</h2>
+      <ul>
         {nodes.map((node) => (
-          <option key={node.id} value={node.areaName}>
-            {node.areaName}
-          </option>
+          <div
+            key={node.id}
+            className="flex cursor-pointer"
+            onClick={() => handleAreaClick(node.areaName)}
+          >
+            <IcOutlineKeyboardArrowDown />
+            <li
+              className={`${
+                selectedArea === node.areaName ? " font-semibold" : ""
+              }`}
+            >
+              {node.areaName}
+              {selectedArea === node.areaName && (
+                <div className="flex">
+                  <MaterialSymbolsArrowForwardIosRounded />
+                  <ul className="pl-4">
+                    {nodes
+                      .filter((node) => node.areaName === selectedArea)
+                      .map((node) => (
+                        <li key={node.id}>{node.cameraName}</li>
+                      ))}
+                  </ul>
+                </div>
+              )}
+            </li>
+          </div>
         ))}
-      </select>
-
-      {selectedArea && (
-        <div className="mt-4">
-          <h2 className="text-lg font-semibold">{selectedArea}</h2>
-          <ul className="list-disc pl-4">
-            {nodes
-              .filter((node) => node.areaName === selectedArea)
-              .map((node) => (
-                <li key={node.id}>{node.cameraName}</li>
-              ))}
-          </ul>
-        </div>
-      )}
+      </ul>
     </div>
   );
 }
--- client/src/admin/pages/Configuration.jsx
+++ client/src/admin/pages/Configuration.jsx
@@ -3,17 +3,19 @@ import Select from "../components/Select";
 
 export default function Configuration() {
   return (
-    <div className="py-6 px-4 grid grid-rows-2 h-full overflow-y-auto">
-      <div className="grid grid-cols-4">
-        <div className="col-span-3">
-          Configuration
-          <img src="https://dummyimage.com/800x800" alt="" />
-        </div>
-        <div className="col-span-1">
-          <Select />
+    <div className="py-6 px-4 flex flex-col h-full overflow-y-auto">
+      <div className="flex-1">
+        <div className="grid grid-cols-4">
+          <div className="col-span-3">
+            Configuration
+            <img src="https://dummyimage.com/800x800" alt="" />
+          </div>
+          <div className="col-span-1">
+            <Select />
+          </div>
         </div>
       </div>
-      <div className="flex flex-col justify-end">
+      <div className="mt-4">
         <Table />
       </div>
     </div>
