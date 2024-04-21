5de70cea9c34cf0422ed9d70a63676c439531813
Ahmad Imran <123238562+ahmadimran2001@users.noreply.github.com>
2024-04-21 18:08:12 +0500

    login page updated

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import iot from "./admin/iot.jpeg";
+import iot from "./admin/background.jpg";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -41,57 +41,33 @@ const Login = () => {
   };
 
   return (
-    <section className="h-screen flex justify-center items-center">
-      <div className="container h-full">
+    <section className="h-screen flex justify-center items-center bg-cover bg-center relative" style={{ backgroundImage: `url(${iot})` }}>
+      <div className="container h-full z-10">
         <div className="flex h-full flex-wrap items-center justify-center">
-          <div className="flex justify-center p-10 border-r border-gray-300">
-            <img src={iot} className="w-96 shadow-xl rounded-lg" alt="IoT" />
-          </div>
-
-          <div className="md:w-8/12 lg:ml-6 lg:w-96">
-            <h2 className="flex justify-center text-2xl font-bold mb-4">
-              Login
+          <div className="md:w-8/12 lg:ml-6 lg:w-96 backdrop-blur-2xl shadow-md rounded-md p-8 border border-white bg-opacity-90">
+            <h2 className="flex justify-center text-3xl font-bold mb-8 text-white">
+              Welcome Back!!!
             </h2>
-            <div className="mb-4">
+            <div className="mb-6">
               <input
                 type="email"
-                className="w-full p-2 border rounded"
+                className="w-full p-4 rounded-lg border border-white focus:outline-none focus:border-black bg-transparent text-white placeholder-gray-400"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 placeholder="Email"
               />
             </div>
-            <div className="mb-4">
+            <div className="mb-6">
               <input
                 type="password"
-                className="w-full p-2 border rounded"
+                className="w-full p-4 rounded-lg border border-white focus:outline-none focus:border-black bg-transparent text-white placeholder-gray-400"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 placeholder="Password"
               />
             </div>
-            {/*<div className="mb-6 flex items-center justify-between">
-              <div className="mb-[0.125rem] block min-h-[1.5rem] pl-[1.5rem]">
-                <input
-                  className="relative float-left -ml-[1.5rem] mr-[6px] mt-[0.15rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ml-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ml-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent dark:border-neutral-600 dark:checked:border-primary dark:checked:bg-primary dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
-                  type="checkbox"
-                  value=""
-                  id="exampleCheck2"
-                />
-                <label
-                  className="inline-block pl-[0.15rem] hover:cursor-pointer"
-                  htmlFor="exampleCheck2"
-                >
-                  Remember me
-                </label>
-              </div>
-
-              <a href="" className="text-blue-500">
-                Forgot password?
-              </a>
-  </div>*/}
             <button
-              className="bg-blue-500 text-white p-2 rounded w-full"
+              className="bg-white text-black p-4 rounded-lg w-full hover:bg-gray-300 transition duration-300 ease-in-out"
               onClick={handleLogin}
             >
               Login
