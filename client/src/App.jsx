97b96334fcf93b3ed2d01690ab320e2c1423cb5c
Ahmad Imran <123238562+ahmadimran2001@users.noreply.github.com>
2024-04-28 14:42:25 +0500

    Protected the Admin and user routes
    User can not access any route without logging in

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Admin from "./admin/Admin";
 import User from "./user/User";
 import AdminLogin from "./admin/pages/AdminLogin";
-import UserLogin from "./user/pages/UserLogin"
+import UserLogin from "./user/pages/UserLogin";
+import AdminPrivateRoute from "./Private_Routes/AdminPrivateRoutes";
+import UserPrivateRoute from "./Private_Routes/UserPrivateRoutes";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/admin/*" element={<Admin />} />
-        <Route path="/user/*" element={<User />} />
+        <Route element={<AdminPrivateRoute />}>
+          <Route path="/admin/*" element={<Admin />} />
+        </Route>
+        <Route element={<UserPrivateRoute />}>
+          <Route path="/user/*" element={<User />} />
+        </Route>
         <Route path="/userLogin/" element={<UserLogin />}/>
         <Route path="/" element={<AdminLogin />} />
       </Routes>
